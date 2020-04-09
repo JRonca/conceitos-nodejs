126fe2432267a9e3072e1243bc09ba2617cccdab
JRonca <jrfr21112000@gmail.com>
2020-04-08 21:08:15 -0300

    projeto finalizado

--- src/app.js
+++ src/app.js
@@ -1,33 +1,79 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  const {title}=request.query;
+  const results=title
+  ? repositories.filter(repository=>repository.title.includes(title))
+  :repositories;
+  return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const {title, url, techs}=request.body;
+  const repository={
+    id:uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const {id}= request.params;
+  const {title,url,techs}=request.body;
+  const repositoryIndex= repositories.findIndex(repository=>repository.id===id);
+  if(repositoryIndex<0){
+    return response.status(400).json({error: '400 - Repository not found'});
+  }
+  const {likes}=repositories[repositoryIndex];
+  const repository={
+    id,
+    title,
+    url,
+    techs,
+    likes,
+  }
+  repositories[repositoryIndex]=repository;
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
+app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const {id}= request.params;
+  const repositoryIndex= repositories.findIndex(repository=>repository.id===id);
+  if(repositoryIndex<0){
+    return response.status(400).json({error: '400 - Repository not found'});
+  }
+  repositories.splice(repositoryIndex,1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+  const {id}= request.params;
+  const repositoryIndex= repositories.findIndex(repository=>repository.id===id);
+  if(repositoryIndex<0){
+    return response.status(400).json({error: '400 - Repository not found'});
+  }
+  repositories[repositoryIndex].likes++;
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
